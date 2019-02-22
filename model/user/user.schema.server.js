4c6e234445b188720ee31cf573e55157b72faae7
vocalid <paarthkotak@gmail.com>
2019-02-22 16:11:25 -0500

    added task, email, group fields

--- model/user/user.schema.server.js
+++ model/user/user.schema.server.js
@@ -2,8 +2,10 @@ var mongoose  = require('mongoose')
 
 var userSchema = mongoose.Schema({
     username: {type: String, required: true},
-    password: {type: String, required: true}
-    // task: {type: mongoose.Schema.Types.ObjectId, ref: 'TaskModel'}
+    password: {type: String, required: true},
+    group: {type: Number, required: true},
+    task: {type: mongoose.Schema.Types.ObjectId, ref: 'TaskModel'},
+    email: {type: String}
 }, {collection: 'user'});
 
 module.exports = userSchema;
